0e6d6a2ca3a9f40bdcbc07e6c36af548398137cf
Raffrerty <raffsimms23@gmail.com>
2022-01-02 22:40:18 +0000

    Favicon

--- src/app.js
+++ src/app.js
@@ -2,18 +2,24 @@ const path = require('path');
 const dotenv = require('dotenv');
 const express = require('express');
 const morgan = require('morgan');
+const favicon = require('serve-favicon');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-dotenv.config({ path: path.join(__dirname, '.env') });
+dotenv.config({
+    path: path.join(__dirname, '.env')
+});
 
 process.chdir(path.join(__dirname, '../src'));
 const publicDirectory = path.join(__dirname, 'public');
 app.use(express.static(publicDirectory));
 
-app.use(morgan('combined'));    
-app.use(express.urlencoded({ extended: false }));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(morgan('combined'));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.json());
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +28,11 @@ app.use('/', require('./routing/getRouting'));
 app.use('/', require('./routing/postRouting'));
 
 app.use((req, res, next) => {
-    res.status(404).sendFile("404.html", { root: publicDirectory });
+    res.status(404).sendFile("404.html", {
+        root: publicDirectory
+    });
 });
 
-app.listen(port, () => {console.log(`Server up on port ${port}`);});
+app.listen(port, () => {
+    console.log(`Server up on port ${port}`);
+});
