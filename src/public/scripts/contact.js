512cf597a757a2bd4fe3baf3d7073d815c773646
Raffrerty <raffsimms23@gmail.com>
2022-01-02 22:40:10 +0000

    Contact form functionality

--- src/public/scripts/contact.js
+++ src/public/scripts/contact.js
@@ -1,17 +1,28 @@
-const form = document.querySelector('#contact-form');
+// const form = document.querySelector('#contact-form');
 
-const sendMail = (mail) => {
-    fetch('sendform', {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'},
-        mode: 'cors',
-        body: JSON.stringify(Object.fromEntries(mail.entries()))
-    }).then((response) => {
-        return response.json();
-    });
-}
+// const sendMail = (mail, _callback) => {
+//     fetch('sendform', {
+//         method: "POST",
+//         headers: {'Content-Type': 'application/json'},
+//         mode: 'cors',
+//         body: JSON.stringify(Object.fromEntries(mail.entries()))
+//     }).then((response) => {
+//         return _callback(response.json());
+//     });
+// }
 
-const formEvent = form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    sendMail(new FormData(form))
-})
+// const formEvent = form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     sendMail(new FormData(form), (response) => {
+//         console.log(response);
+//     })
+// })
+
+grecaptcha.ready(function () {
+    grecaptcha.execute('6LfdVt0ZAAAAAASyUX8YSa1YoXjWBFn5L22OFYrk', {
+            action: 'validate_captcha'
+        })
+        .then(function (token) {
+            document.getElementById('g-recaptcha-response').value = token;
+        });
+});
--- src/routing/postRouting.js
+++ src/routing/postRouting.js
@@ -1,23 +1,96 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+const fetchUrl = require('fetch').fetchUrl;
 
 const validateEmail = (email) => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(String(email).toLowerCase());
 }
 
-router.post('/sendform', (req, res) => {
+const sendResponse = (res, status, message) => {
+    res.render('contact', {
+        status: status,
+        message: message
+    })
+};
 
-    const { nameInput, emailInput, subjectInput, messageInput } = req.body;
+const sendEmail = (name, email, subject, message) => {
+    const GMAIL_USER = process.env.GMAIL_USER
+    const GMAIL_PASS = process.env.GMAIL_PASS
+    const RECIPIENT_EMAIL = 'raff@raffsimms.com'
+
+    const smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: GMAIL_USER,
+            pass: GMAIL_PASS
+        }
+    })
+
+    const mailOptions = {
+        from: `${name}`,
+        to: RECIPIENT_EMAIL,
+        subject: `${subject}`,
+        text: `Name: ${name}\nEmail: ${email}\n\n${message}`
+    }
+
+    smtpTrans.sendMail(mailOptions, (err, response) => {
+        if (err) {
+            console.error(err);
+            return false;
+        } else {
+            return true;
+        }
+    })
+}
+
+router.post('/sendform', (req, res) => {
+    const {
+        nameInput,
+        emailInput,
+        subjectInput,
+        messageInput
+    } = req.body;
+
+    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+        return sendResponse(res, false, "Failed captcha verification");
+    }
+
+    const SECRET_KEY = process.env.SECRET_KEY;
+    let ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.ip;
+    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + SECRET_KEY + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + ip;
+
+    let validCaptcha;
+    fetchUrl(verificationURL, (error, response, body) => {
+        body = JSON.parse(body.toString())
+        if (body.success !== undefined && !body.success) {
+            validCaptcha = false;
+        } else {
+            validCaptcha = true;
+        }
+    })
+    if (!validCaptcha) {
+        return sendResponse(res, false, "Failed captcha verification");
+    }
+
     if (nameInput == "" || emailInput == "" || subjectInput == "" || messageInput == "") {
-        return;
+        return sendResponse(res, false, "Missing field");
     }
     if (!validateEmail(emailInput)) {
-        return;
+        return sendResponse(res, false, "Invalid email");
     }
-    console.log(nameInput, emailInput, subjectInput, messageInput);
+
+    sendEmail(nameInput, emailInput, subjectInput, messageInput, (response) => {
+        if (response) {
+            sendResponse(res, true, "Message sent");
+        } else {
+            sendResponse(res, false, "Failed to send message")
+        }
+    });
 
 });
-
 
 module.exports = router;
