ddeeab974b9befe26427ee0d22cfd4300bebdc65
Raffrerty <raffsimms23@gmail.com>
2022-01-06 15:46:29 +0000

    Dry Principle

--- src/routing/getRouting.js
+++ src/routing/getRouting.js
@@ -1,4 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
+
+function fetchData(type) {
+    
+    let fileName;
+    switch (type) {
+        case "projects":
+            fileName = "projects";
+            break
+        default:
+            break
+    }
+    if (fileName != null || filename != undefined) {
+        return JSON.parse(fs.readFileSync(path.join(__dirname, `../data/${fileName}.json`)));
+    }
+}
 
 router.get('/', (req, res) => {
     res.status(200).render('index');
@@ -9,7 +26,10 @@ router.get('/contact', (req, res) => {
 });
 
 router.get('/projects', (req, res) => {
-    res.status(200).render('projects');
+    res.status(200).render('projects', {
+        "projects": fetchData("projects").projects
+    });
 });
+
 
 module.exports = router;
