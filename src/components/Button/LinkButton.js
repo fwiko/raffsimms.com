826415bcfb604c9f1e732ed287ec92a42969d797
Rafferty Simms <raffsimms23@gmail.com>
2023-08-26 10:14:45 +0100

    Rounded icon buttons in header

--- src/components/Button/LinkButton.js
+++ src/components/Button/LinkButton.js
@@ -8,7 +8,7 @@ export default function LinkButton({ children, href, title, internal = false }) 
         internal ? (
             <Link href={href} title={title} passHref><span className={`${styles.btn} ${styles['btn-primary']}`}>Go back home</span></Link>
         ) : (
-            <a href={href} target="_blank" rel="noreferrer" title={title} className={`${styles.btn} ${styles['btn-primary']}`}>
+            <a href={href} target="_blank" rel="noreferrer" title={title} className={`${styles.btn} ${styles['btn-primary']} ${styles['btn-icon'] ? styles['btn-icon'] : ''}`}>
                 {children}
             </a >
         )
