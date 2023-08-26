993820ea2a9dddb0013e53cf922d74f7058223f9
Rafferty Simms <raffsimms23@gmail.com>
2023-08-26 10:08:45 +0100

    Removed original icon button

--- src/components/Button/IconButton.js
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.scss'
-
-export default function IconButton({ onClick, icon }) {
-    return (
-        <button rel="noreferrer" onClick={onClick} className={`${styles['btn']} ${styles["btn-icon"]} text-clr-light `}>
-            <i className={icon} />
-        </button>
-    );
-}
