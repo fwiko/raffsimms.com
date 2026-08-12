35df9ffd52e15fae8405d9f735af795ae2ee4313
fwiko <raffsimms@gmail.com>
2026-08-12 14:02:21 +0100

    I got my domain back from the Taiwanese squatters

--- src/components/Tag/Tag.js
+++
@@ -1,9 +0,0 @@
-import styles from './Tag.module.scss';
-
-export default function Tag({ text }) {
-    return (
-        <div className={`${styles.tag} font-weight-bold`}>
-            {text}
-        </div>
-    )
-}
